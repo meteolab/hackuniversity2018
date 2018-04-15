11018e17c80e54de48da0ed4e378de3b3c3c88e2
Vladimir V. Chukin <chukin@meteolab.ru>
2018-04-15 12:16:06 +0300

    MQTT messages are added

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,6 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include <meteonet.h>
 #include <PMS.h>
 
@@ -19,8 +19,8 @@
  *  ------------------
  *  VCC (1) -> 5V
  *  GND (3) -> GND
- *  RX  (7) -> D3 (0)
- *  TX  (9) -> D4 (2)
+ *  RX  (7) -> TX (1) // D3 (0)
+ *  TX  (9) -> RX (3) // D4 (2)
  *  
  *  DS18B20 -> ESP8266
  *  ------------------
@@ -34,17 +34,19 @@
  *  (-) -> GND
  */
 
-SoftwareSerial PMS7003(2, 0); // (RX = pin 2, TX = pin 0)
-PMS pms(PMS7003);
+//SoftwareSerial PMS7003(2, 0); // (RX = pin 2, TX = pin 0)
+//PMS pms(PMS7003);
+//PMS::DATA data;
+PMS pms(Serial); // (RX = pin 3, TX = pin 1)
 PMS::DATA data;
-OneWire           oneWire(4);
+OneWire oneWire(4);
 DallasTemperature sensors(&oneWire);
 int pinLED = 5;
-String            Channel = "1234567890";
-Meteonet          meteonet(Channel);
+String Channel = "hack2018";
+Meteonet meteonet(Channel);
 unsigned long rem;
 unsigned long prev = 0;
-unsigned long period = 30;
+unsigned long period = 60;
 double summa_T = 0.0;
 double summa_1_0 = 0.0;
 double summa_2_5 = 0.0;
@@ -59,29 +61,19 @@ double PM_10_0;
 
 void setup() {
     Serial.begin(9600);
-    PMS7003.begin(9600);
+    // PMS7003.begin(9600);
     delay(500);
-    Serial.println();
-    // meteonet.begin();
+    meteonet.begin();
     delay(500);
     pinMode(pinLED, OUTPUT);
 }
 
 
 void loop() {
+    // N = 1; // !!!
 
     if(pms.read(data)) {
         digitalWrite(pinLED, HIGH);
-        /*
-        Serial.println("Data:");
-        Serial.print("PM 1.0 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_1_0);
-        Serial.print("PM 2.5 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_2_5);
-        Serial.print("PM 10.0 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_10_0);
-        Serial.println();
-        */
         sensors.requestTemperatures();
         summa_T    += sensors.getTempCByIndex(0)+273.15;
         summa_1_0  += data.PM_AE_UG_1_0;
@@ -95,28 +87,22 @@ void loop() {
     // Find seconds from last measurement
     rem = meteonet.getTimestamp() % period;
     if(rem < prev && N > 0) {
+        // sensors.requestTemperatures();
+        // T = sensors.getTempCByIndex(0)+273.15;
         T       = summa_T/N;
         PM_1_0  = summa_1_0/N;
         PM_2_5  = summa_2_5/N;
         PM_10_0 = summa_10_0/N;
+        meteonet.send(PM_2_5);
+        meteonet.loop();
         /*
-        Serial.print(meteonet.getTimestamp());
-        Serial.print("\t");
-        Serial.print(meteonet.getLatitude());
-        Serial.print("\t");
-        Serial.print(meteonet.getLongitude());
-        Serial.print("\t");
-        Serial.println(PM25);
-        meteonet.send(PM25);
-        //meteonet.loop();
-        */
-        Serial.print(T);
         Serial.print("\t");
         Serial.print(PM_1_0);
         Serial.print("\t");
         Serial.print(PM_2_5);
         Serial.print("\t");
         Serial.println(PM_10_0);
+        */
         summa_T    = 0.0;
         summa_1_0  = 0.0;
         summa_2_5  = 0.0;
